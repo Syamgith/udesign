141fc5194148bc0801cc6c74d95b8524a6b72854
syamgith <syamgithksudheesh@gmail.com>
2021-07-19 23:35:47 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,31 +3,30 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appTitle),
-        ),
-        body: MyCustomForm(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
       ),
+      body: MyCustomForm(),
     );
   }
 }
+
 class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
     return MyCustomFormState();
   }
 }
+
 class MyCustomFormState extends State<MyCustomForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16)
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: TextField(
             decoration: InputDecoration(
               border: OutlineInputBorder(),
@@ -36,38 +35,32 @@ class MyCustomFormState extends State<MyCustomForm> {
           ),
         ),
         Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              TextField(
-               obscureText: true,
-               decoration: InputDecoration(
-                 border: OutlineInputBorder(),
-                labelText: 'Password',
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
                 ),
-              ),
-            ],
-          )
-        ),
+              ],
+            )),
         Container(
-            height: 50,
-            width: 350,
-            decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              
-            child: FlatButton(
-              onPressed: () {},
-              child: Text(
-                'Submit',
-                style: TextStyle(color: Colors.amber, fontSize: 25),
-                ),
- 
-              ),
- 
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          height: 50,
+          width: 350,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Submit',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
         ),
- 
       ],
     );
   }
 }
-
