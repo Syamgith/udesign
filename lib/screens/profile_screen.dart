95a66f8d73b23c1fdf58c1159a0a66630e59a9b9
syamgith <syamgithksudheesh@gmail.com>
2021-07-24 23:11:37 +0530

    profile page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:udesign/components/drawer_widget.dart';
 import 'package:udesign/resources/style_resourses.dart';
 import 'package:udesign/screens/login_screen.dart';
 import 'package:udesign/screens/register_screen.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  Color color1 = Colors.amber;
+  Color color2 = Colors.blue;
+  Color color3 = Colors.red;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +26,126 @@ class ProfileScreen extends StatelessWidget {
           style: StyleResourse.AppBarTitleStyle,
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Center(child: Text('Hi guest!')),
-          loginOrRegisterButton(context),
-        ],
+      body: userUI(context),
+    );
+  }
+
+  Widget userUI(context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: MediaQuery.of(context).size.width / 4,
+              child: Text(
+                "Name",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text("Name"),
+            Divider(),
+            SizedBox(height: 16),
+            Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Center(child: Text('Select your favorite colors')),
+                    onTap: () {},
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                          icon: Icon(
+                            Icons.circle,
+                            color: color1,
+                            size: 35,
+                          ),
+                          onPressed: () {
+                            selectColor(1);
+                          }),
+                      IconButton(
+                          icon: Icon(
+                            Icons.circle,
+                            color: color2,
+                            size: 35,
+                          ),
+                          onPressed: () {
+                            selectColor(2);
+                          }),
+                      IconButton(
+                          icon: Icon(
+                            Icons.circle,
+                            color: color3,
+                            size: 35,
+                          ),
+                          onPressed: () {
+                            selectColor(3);
+                          }),
+                    ],
+                  ),
+                  ListTile(
+                    title: Text('fav items'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
-    ;
+  }
+
+  void selectColor(i) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick Color'),
+          content: SingleChildScrollView(
+            child: BlockPicker(
+              pickerColor: i == 1
+                  ? color1
+                  : i == 2
+                      ? color2
+                      : color3,
+              onColorChanged: (color) {
+                this.setState(() {
+                  if (i == 1) {
+                    color1 = color;
+                  } else if (i == 2) {
+                    color2 = color;
+                  } else if (i == 3) {
+                    color3 = color;
+                  }
+                });
+              },
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Ok"))
+          ],
+        );
+      },
+    );
+  }
+
+  Widget guestUI(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Center(child: Text('Hi guest!')),
+        loginOrRegisterButton(context),
+      ],
+    );
   }
 
   Widget loginOrRegisterButton(context) {
