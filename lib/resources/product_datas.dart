5ba90779656b5765bb0c670dea9ca40d5ec831cd
syamgith <syamgithksudheesh@gmail.com>
2021-07-27 16:20:54 +0530

    edited products

--- lib/resources/product_datas.dart
+++ lib/resources/product_datas.dart
@@ -3,158 +3,32 @@ import 'package:udesign/models/product_model.dart';
 class ProductDatas {
   static List<Product> productsList = [
     Product(
-        title: 'Coloured Sofa',
+        title: 'Colour Sofa',
         imgUrl:
             'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
         model: Model3D(
           address: 'couch.sfb',
         )),
     Product(
-        title: 'test',
+        title: 'Piano',
         imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'Wal_Clok.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'table.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'tredmill.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'table2.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'StylishDesk.sfb',
-        )),
-    Product(
-        title: 'sofa',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'sofa.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'school_chair.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'potted_plant.sfb',
-        )),
-    Product(
-        title: 'piano',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
+            'https://kawaius.com/wp-content/uploads/2018/04/GL50-Polished-Ebony-450x450.jpg',
         model: Model3D(
           address: 'piano.sfb',
         )),
     Product(
-        title: 'test',
+        title: 'Tredmill',
         imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
+            'https://3.imimg.com/data3/LR/PI/MY-2184756/lifeline-motorised-treadmill-250x250.jpg',
         model: Model3D(
-          address: 'mac.sfb',
+          address: 'tredmill.sfb',
         )),
     Product(
-        title: 'test',
+        title: 'Wide floor sofa',
         imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
+            'https://i.pinimg.com/originals/9a/e7/07/9ae707a786355bd292a8a8eca65c2079.jpg',
         model: Model3D(
-          address: 'coffee_table.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://i.pinimg.com/originals/cc/5e/31/cc5e311fba93e4d2da4a25f04e9bb212.png',
-        model: Model3D(
-          address: 'chair.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/SheenChair/screenshot/screenshot.jpg',
-        model: Model3D(
-          address: 'clock1.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/SheenChair/screenshot/screenshot.jpg',
-        model: Model3D(
-          address: 'monitor.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/SheenChair/screenshot/screenshot.jpg',
-        model: Model3D(
-          address: 'freemodel.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/wall-clock/t/x/q/wall-clock-w0001pa01-analog-titan-original-imafgnyzufgmvafk.jpeg?q=70',
-        model: Model3D(
-          address: 'wallclock.sfb',
-        )),
-    // Product(
-    //     title: 'test',
-    //     imgUrl:
-    //         'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/wall-clock/t/x/q/wall-clock-w0001pa01-analog-titan-original-imafgnyzufgmvafk.jpeg?q=70',
-    //     model: Model3D(
-    //       address: 'lamp.sfb',
-    //     )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/wall-clock/t/x/q/wall-clock-w0001pa01-analog-titan-original-imafgnyzufgmvafk.jpeg?q=70',
-        model: Model3D(
-          address: 'light.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/wall-clock/t/x/q/wall-clock-w0001pa01-analog-titan-original-imafgnyzufgmvafk.jpeg?q=70',
-        model: Model3D(
-          address: 'lamp2.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/wall-clock/t/x/q/wall-clock-w0001pa01-analog-titan-original-imafgnyzufgmvafk.jpeg?q=70',
-        model: Model3D(
-          address: 'lamp3.sfb',
-        )),
-    Product(
-        title: 'test',
-        imgUrl:
-            'https://rukminim1.flixcart.com/image/416/416/jyeq64w0/wall-clock/t/x/q/wall-clock-w0001pa01-analog-titan-original-imafgnyzufgmvafk.jpeg?q=70',
-        model: Model3D(
-          address: 'clock2.sfb',
+          address: 'sofa.sfb',
         )),
     Product(
         title: 'Glam Sofa',
@@ -173,6 +47,27 @@ class ProductDatas {
           address:
               'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/SheenChair/glTF/SheenChair.gltf',
           isRemote: true,
+        )),
+    Product(
+        title: 'Wall light',
+        imgUrl:
+            'https://www.luxurylighting.co.uk/ekmps/shops/luxurylighting/images/huguenot-lake-single-bathroom-wall-light-feiss-lighting-62097-p.jpg',
+        model: Model3D(
+          address: 'light.sfb',
+        )),
+    Product(
+        title: 'Table lamp',
+        imgUrl:
+            'https://www.homesrus.ae/media/catalog/product/cache/a804bc9a22f55bdcc68786db3543c068/5/1/5110100200806_1_3.jpg',
+        model: Model3D(
+          address: 'lamp3.sfb',
+        )),
+    Product(
+        title: 'Silver pot',
+        imgUrl:
+            'http://www.artnet.com/WebServices/images/ll01361lldREbJFg8ZECfDrCWvaHBOcBpoF/buccellati-(co.)-buccellati-sterling-silver-vase.jpg',
+        model: Model3D(
+          address: 'potted_plant.sfb',
         )),
   ];
 }
